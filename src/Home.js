2e93a105c6e168a2d6da14e2d491efd44a57d6e8
nani <vnanikalyan@gmail.com>
2019-04-18 09:20:49 +0530

    This is my first commit to Git

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return <div>
+                    This is my first Component
+                </div>
+        
+    }
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+import Home from './Home'
+
+ReactDOM.render(<Home />, document.getElementById('root'));
