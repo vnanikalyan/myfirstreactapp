26b6b92b35a02cbbd712e4fe0fe53bae7ad29630
nani <vnanikalyan@gmail.com>
2019-05-07 11:43:00 +0530

    Youtube component & loader

--- src/containers/StateDemo/StateDemo.js
+++ src/containers/StateDemo/StateDemo.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Youtube from 'react-youtube';
+import axios from 'axios';
+import Loader from 'react-loader-spinner'
 
 class demo extends Component{
     state = {
         count:null   ,
-        img: []
+        images: null,
+        videoID: null
     }
 
     increaseCnt = x => {
@@ -33,6 +37,26 @@ class demo extends Component{
             ...this.state,
             count: x.count
         })
+
+        axios.get('https://s3.ap-south-1.amazonaws.com/www.capstonechurch.in/announcements.json')
+            .then( (response) => {
+                //console.log(response.data.images);                
+                let images = response.data.images;
+                let liveUrl = response.data.liveurl;                
+
+                images.map(i => {
+                    console.log(i.image);
+                });
+
+                this.setState({
+                    ...this.state,
+                    videoID: liveUrl,
+                    images: images
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
     
     render() {
@@ -40,6 +64,32 @@ class demo extends Component{
                     <p>{this.state.count}</p>
                     <button onClick={this.increaseCnt}>Increment</button>
                     <button onClick={this.decreaseCnt}>Decrement</button>
+                    
+                    {this.state.videoID !== null ?
+                        <Youtube
+                            videoId={this.state.videoID}>
+                        </Youtube>
+                        :
+                        <Loader 
+                            type="Circles"
+                            color="#00BFFF"
+                            height="100"	
+                            width="100"
+                        />  
+                    }                   
+
+                    { this.state.images !== null ?
+                        this.state.images.map(i =>{
+                            return <img src={i.image} alt="" height="100" width="150"></img>
+                        })
+                        :
+                        <Loader 
+                            type="Circles"
+                            color="#00BFFF"
+                            height="100"	
+                            width="100"
+                        />                        
+                    }
                 </div>
     }
 }
