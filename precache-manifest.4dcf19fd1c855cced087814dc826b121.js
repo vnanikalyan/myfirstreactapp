f3fa7c5d676051ac05d93d9aefda6e7bbbcf0131
nani <vnanikalyan@gmail.com>
2019-05-07 16:33:42 +0530

    Updates

---
+++ precache-manifest.4dcf19fd1c855cced087814dc826b121.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "75c46ddc97e36d10a67b",
+    "url": "/vnanikalyan/myfirstreactapp/static/css/main.99768814.chunk.css"
+  },
+  {
+    "revision": "75c46ddc97e36d10a67b",
+    "url": "/vnanikalyan/myfirstreactapp/static/js/main.da830217.chunk.js"
+  },
+  {
+    "revision": "12d834c95f23e6ed5213",
+    "url": "/vnanikalyan/myfirstreactapp/static/js/runtime~main.b18463ae.js"
+  },
+  {
+    "revision": "3fd89cd9645a4cfb4984",
+    "url": "/vnanikalyan/myfirstreactapp/static/css/2.3e25e868.chunk.css"
+  },
+  {
+    "revision": "3fd89cd9645a4cfb4984",
+    "url": "/vnanikalyan/myfirstreactapp/static/js/2.fd27a0ce.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vnanikalyan/myfirstreactapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1a3a3161bc9b58607ac67c1bfc0b9873",
+    "url": "/vnanikalyan/myfirstreactapp/index.html"
+  }
+];
